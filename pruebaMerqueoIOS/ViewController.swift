8db8b3256ef0c27dd733c4b4cf929a8aa028e646
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2022-02-08 09:19:37 -0500

    Initial Commit

---
+++ pruebaMerqueoIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pruebaMerqueoIOS
+//
+//  Created by Oscar David Myerston Vega on 8/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
